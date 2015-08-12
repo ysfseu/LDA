30cd74f268c0e9e93cbce75bf6c77b7ec03c592f
ysfseu <ysfseu@lotuseed.com>
2015-08-12 10:54:52 +0800

    show terms for each topic

--- lda.R
+++ lda.R
@@ -18,20 +18,36 @@ dtm<-DocumentTermMatrix(corpus,control=list(wordLengths=c(1,Inf)))
 #delete rows without terms
 rowTotals <- apply(dtm , 1, sum)
 dtm<- dtm[rowTotals> 0, ] 
-k = 4
-lda.model = LDA(dtm, k)
-terms(lda.model,20)
+k = 5
+lda.model = LDA(dtm, k,control = list(em = list(iter.max = 1000, tol = 10^-4)))
+
 apps.topics=posterior(lda.model,dtm)$topics
 df.apps.topics=as.data.frame(apps.topics)
 df.apps.topics = cbind(app=as.character(rownames(df.apps.topics)), 
                        df.apps.topics, stringsAsFactors=F)
 #get the document which is above 60% related to topic 1
-sample(which(df.apps.topics$"1" > .6), 10)
+sample(which(C > .6), 10)
 # predict
 test<-dtm[1:100,]
 test.topics <- posterior(lda.model,test)
 # select the topic with the highest proportion.
 (test.topics <- apply(test.topics$topics, 1, which.max))
 
+#Get the nt highest terms for each topic.And map the terms' id to its real names.
+nt<-5
+apps.terms<-terms(lda.model,nt)
+map<-read.table("./data/map",encoding = "UTF-8",sep=' ',as.is = TRUE)
+names(map)<-c("id","name")
+apps.terms<-data.frame(apps.terms)
+index<-data.frame(matrix(ncol = k,nrow = nt))
+for(i in 1:k)
+{
+  index[,i]<-match(apps.terms[,i],map$id)
+}
+topicmap<-NULL
+for(i in 1:k)
+{
+  topicmap<-cbind(topicmap,map[index[,i],2])
+}
 
 
